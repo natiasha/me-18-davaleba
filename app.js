f7f5c474ec049309db45cc19f1931899e5798880
natia <shavladze.natia@teachers.gov.ge>
2021-11-08 15:07:57 +0400

    Update app.js

--- app.js
+++ app.js
@@ -19,3 +19,27 @@ i="0"+i;
 return i;
 }
 //2
+//3
+//4
+//4
+const students = [-1, -3, 4, 2];
+let minstudents=3;
+let studentcount=0;
+for (i=0; i<=4; i++)
+{
+if (students[i]<=0)
+{
+  studentcount=studentcount+1;
+  console.log(studentcount);
+  
+}
+}
+if (studentcount - minstudents>=0)
+{
+  console.log('Jes');
+}
+else
+{
+  console.log('no');
+}
+
