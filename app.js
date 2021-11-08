ec3e3b1e72ec0a50442a7aa0f711c8c70e5fe434
natia <shavladze.natia@teachers.gov.ge>
2021-11-08 16:07:15 +0400

    add N2

--- app.js
+++ app.js
@@ -19,8 +19,86 @@ i="0"+i;
 return i;
 }
 //2
+const slides = document.querySelectorAll('.slide-item');
+const slidesLength = slides.length;
+const nextButton = document.querySelector('#next');
+const prevButton = document.querySelector('#prev');
+const stopBtn = document.querySelector('#stop-sliding');
+const startBtn = document.querySelector('#start-sliding');
+let activeIndex = 0;
+
+function renderSlider() {
+  slides.forEach((element, index) => {
+    element.style.transform = `translateX(${100 * (index - activeIndex % slidesLength)}%)`;
+  })
+}
+
+renderSlider();
+
+function nextSlide() {
+  if(activeIndex === (slidesLength - 1)){
+    activeIndex = 0;
+  } else {
+    activeIndex = activeIndex + 1;
+  }
+
+  renderSlider();
+}
+
+function prevSlide() {
+  if(activeIndex === 0){
+    activeIndex = slidesLength - 1;
+  } else {
+    activeIndex = activeIndex - 1;
+  }
+
+  renderSlider();
+}
+
+nextButton.addEventListener('click', (e) => {
+  nextSlide();
+});
+prevButton.addEventListener('click', prevSlide);
+
+document.addEventListener('keydown', (e) => {
+  console.log(e.code);
+  if(e.code === 'ArrowRight'){
+    nextSlide();
+  }
+  if(e.code === 'ArrowLeft'){
+    prevSlide();
+  }
+});
+
+let intervalId = null;
+
+function startAutoSliding() {
+  if(!intervalId){
+    intervalId = setInterval(() => {
+      nextSlide();
+    }, 3000);
+  }
+}
+
+function stopAutoSliding() {
+  clearInterval(intervalId);
+  intervalId = null;
+}
+stopBtn.addEventListener('click', stopAutoSliding);
+startBtn.addEventListener('click', startAutoSliding);
+
+//damatebuli kodi
+let enterEventCount = 0;
+let leaveEventCount = 0;
+const mouseTarget = document.getElementById('slide-item');
+mouseTarget.addEventListener('mouseenter', e => {
+   enterEventCount++;
+}
+mouseTarget.addEventListener('mouseleave', e => {
+    leaveEventCount++;
+   }
+
 //3
-//4
 //4
 const students = [-1, -3, 4, 2];
 let minstudents=3;
